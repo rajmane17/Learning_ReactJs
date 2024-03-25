6c86808c35cab5d1f484a15803a8c8a25107d00e
Raj_Mane <142428026+rajmane17@users.noreply.github.com>
2024-03-25 12:49:16 +0000

    Made the Navbar

--- PortFolio/src/components/Navbar.jsx
+++ PortFolio/src/components/Navbar.jsx
@@ -1,11 +1,45 @@
 import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
-
+import { useState, useEffect } from 'react'
+import {styles} from "../styles"
+import {navLinks} from "../Constant"
+import {logo, menu, close} from "../assets"
 
 const Navbar = () => {
-  return (
-    <nav>
 
+  const [active,setActive] = useState("")
+  return (
+    <nav className={`
+    ${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
+      <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
+        <Link 
+        to="/"
+        className='flex items-center gap-2'
+        onClick={() => {
+          setActive("");
+          window.scroll(0,0);
+        }}>
+          <img src={logo} alt='My Logo' className='w-9 h-9 object-containe'/>
+          <p className='text-white font-bold text-[18px] cursor-pointer'>RAJ <span className='sm:block hidden'>| JAVASCRIPT MASTERY</span></p> 
+        </Link>
+        <ul className='list-none hidden sm:flex flex-row gap-10'>
+            {navLinks.map((link)=> {
+              // we are not getting the links over here
+              <li
+              key={link.id}
+              className={`${
+                active === link.title ? "text-white" : "text-secondary"
+
+              } hover:text-white text-[18px] cursur-pointer font-medium` }
+              onClick={()=> {
+                setActive(link.title)
+              }}
+              >
+                <a href={`#${link.id}`}>{link.title}</a>
+              </li>
+            })}
+          </ul>
+      </div>
     </nav>
   )
 }
