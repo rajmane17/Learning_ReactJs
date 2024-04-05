03da3bbe3d08c6de4b140593d9d43a6a8cae9cb5
Raj_Mane <142428026+rajmane17@users.noreply.github.com>
2024-04-05 12:17:09 +0000

    basic setup of QuizApp is done

---
+++ QuizApp/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Header from "./Components/Header"
+
+function App() {
+
+  return (
+    <>
+    <h1>Quiz App</h1>
+    <Header />
+    </>
+  )
+}
+
+export default App
---
+++ QuizApp/src/Components/Header.jsx
@@ -0,0 +1,12 @@
+import react from "react"
+// import { Link } from "react-router-dom"
+
+function Header() {
+    return(
+        <>
+            Quiz App
+        </>
+    )
+}
+
+export default Header
